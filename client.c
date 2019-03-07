a72908f9db53280c4ee2fee37f03956cd5627c5e
Flo Yao <floxyao@gmail.com>
2019-03-07 15:45:05 -0800

    Add files via upload

--- client.c
+++ client.c
@@ -1,120 +1,176 @@
-// Client side C/C++ program to demonstrate Socket programming 
+//===============================================================================================
+//HW 2:     Socket Programming
+//Author:   Florence Yao
+//Course:   CECS-327
+//Date:     2-25-19
+//Info:     Client side C/C++ program to demonstrate Socket programming. TCP is changed to non-
+//          blocking socket and runs a user input process in a separate thread. Sleep is used
+//          due to the loop running too fast to type inputs
+//===============================================================================================
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h>
 #include <unistd.h>
+#include <fcntl.h>
+#include <pthread.h>
+#define SENDER_NAME "client: "
+#define IP_ADDR "127.0.0.1"
 #define PORT 8080 
+#define NUM_THREADS 2
+#define MSG_LEN 100
+#define BUF_SIZE 1024
 
+//GLOBAL
+int       sock;
+struct    sockaddr_in client_addr;
+struct    sockaddr_in serv_addr;     
+socklen_t CLADDR_LEN = sizeof(client_addr);
+int       inet_pton(); //get rid of warning
+char      msg[MSG_LEN];
+char      buffer[BUF_SIZE] = {0};
+
+//==========================================================================
+// Concatenation of two strings
+//==========================================================================
+void concat(char* p, char *q){
+   int c, d;
+   c = 0;
+   while (p[c] != '\0') {
+      c++;      
+   }
+   d = 0;
+   while (q[d] != '\0') {
+      p[c] = q[d];
+      d++;
+      c++;    
+   }
+   p[c] = '\0';
+}
+
+//==========================================================================
+// User Input Thread
+// Gets the message input from user.
+//==========================================================================
+void* input_thread(void* arg){
+    for(;;){
+        char label[] = SENDER_NAME;
+        fgets(msg, MSG_LEN, stdin);
+        concat(label, msg); //concat label to message to identify sender
+        strcpy(msg, label);
+    }
+    pthread_exit(NULL);
+    return NULL;
+}
+
+//==========================================================================
+// Send/Receive Socket
+// Sends and receives messages to client. 
+// *note sleep() is used because without it, the loop appears to be too fast
+// and messages aren't being sent correctly
+//==========================================================================
+void* message_thread(void* arg){
+    for(;;){
+        int valread = recv( sock , buffer, BUF_SIZE, 0);
+        send(sock , msg , strlen(msg) , 0);
+        memset(msg, 0, MSG_LEN);                      //clear message
+        printf("%s",buffer);                          //display message
+        bzero(buffer, sizeof(buffer));                //flush buffer
+        sleep(1);                                     //introduce delay or else loops too fast (?) 
+    }
+    close(sock); 
+    pthread_exit(NULL);
+    return NULL; //silence
+}
+
+//==========================================================================
+// Main connects sockets, creates and join threads
+// notes: AF_INET = domain of socket
+//        SOCK_STREAM = type of socket (TCP/UDP)
+//        0 = default protocol, TCP   */
+//==========================================================================
 int main(int argc, char const *argv[]) 
-{ 
-	struct sockaddr_in client_addr;
-    socklen_t CLADDR_LEN = sizeof(client_addr);
-	int sock = 0, valread;
-    
-    int inet_pton();                  //get rid of warning
-    struct sockaddr_in serv_addr;     //This structure contains fields address and port
-
-    
-    //===================================================
-    // Creating socket file descriptor
-    // Creates one of the end points needed for communication
-    // AF_INET     = domain of socket
-    // SOCK_STREAM = type of socket (TCP/UDP-datagram)
-    // 0           = default protocol, TCP
-    //===================================================
+{
+    /*---------------------------------
+     Creating socket file descriptor
+    ----------------------------------*/
 	if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0)
 	{ 
 		printf("\n Socket creation error \n"); 
 		return -1; 
-	} 
-
+	}
+    
 	memset(&serv_addr, '0', sizeof(serv_addr));
     
-    //===================================================
-    // Specify address for socket to connect to
-    // Specify family of address so it knows what type of address
-    // Specify port; htons() converts the PORT to correct data format
-    // so structure understands the port number and where we need connect to
-    //===================================================
+    /*---------------------------------------------------------------------
+     Specify address for socket to connect to
+     Specify family of address so it knows what type of address
+     Specify port; htons() converts the PORT to correct data format
+     so structure understands the port number and where we need connect to
+    ----------------------------------------------------------------------*/
 	serv_addr.sin_family = AF_INET;
     serv_addr.sin_port = htons(PORT);
     
-    //===================================================
-	// Convert IPv4 and IPv6 addresses from text to binary form
-    // serv_addr = struct holding info about address
-    // sin_addr is a struct itself containing the address itself
-    //===================================================
-    if(inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr)<=0)
+    /*----------------------------------------------------------
+	 Convert IPv4 and IPv6 addresses from text to binary form
+     serv_addr = struct holding info about address
+     sin_addr is a struct itself containing the address itself
+    -----------------------------------------------------------*/
+    if(inet_pton(AF_INET, IP_ADDR, &serv_addr.sin_addr)<=0)
 	{ 
 		printf("\nInvalid address/ Address not supported \n"); 
 		return -1; 
 	} 
 
-    //===================================================
-    // Connect to the socket
-    // sock      = our socket
-    // serv_addr = cast our address to the right struct type
-    // size      = size of addr
-    // Returns an integer to indicate success/failure
-    //===================================================
+    /*-------------------------------------------------------
+     Connect to the socket
+     sock      = our socket
+     serv_addr = cast our address to the right struct type
+     size      = size of addr
+     Returns an integer to indicate success/failure
+    -------------------------------------------------------*/
 	if (connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr)) < 0) 
 	{ 
 		printf("\nConnection Failed \n"); 
 		return -1; 
     }
     else{
-        printf("\nConnection Established\n\n");
+        printf("\nConnection Established");
+        printf("\nIP: %s",IP_ADDR);
+        printf("\nPORT: %d\n\n",PORT);
     }
     
-//    char server_response[256];
-//    recv(sock, &server_response, sizeof(server_response), 0);
+    /*-----------------------------------
+     Change the socket to non-blocking
+    -----------------------------------*/
+    fcntl(sock, F_SETFL, O_NONBLOCK);
     
-//    else{
-//        printf("\nConnection Established\n");
-//
-//        printf("IP address is: %s\n", inet_ntoa(client_addr.sin_addr));
-//        printf("port is: %d\n", (int) ntohs(client_addr.sin_port));
-//        //inet_ntop(AF_INET, (struct sockaddr_in *)&(client_addr.sin_addr), client_addr, CLADDR_LEN);
-//        //char *c = inet_ntop(AF_INET, (struct sockaddr_in *)&client_addr, &serv_addr, CLADDR_LEN);
-//        //printf("IP: %d", *c);
-//        //printf("\nPort: %d", PORT);
-//    }
-    
-    //char *hello = "Hello from client";
-    //char msg[100];
-    char msg[50];
-    char buffer[1024] = {0};
-    //char buffer[1024] = {0};
-    
-    
-    for(;;){
-        //put message in buffer (send message to server)
-        printf(":");
-        scanf ("%[^\n]%*c", msg);
-        
-        send(sock , msg , strlen(msg) , 0 );
-        
-//        if((len = recv(sock, buffer, sizeof(buffer), 0)) > 0) {
-//            buffer[len] = '\0';
-//            fputs(buffer, stdout);
-//        }
-        
-        //wait for message in buffer (wait for response)
-        //printf("server: ");
-        //valread = read( sock , buffer, 1024);
-        
-        //print buffer
-        //printf("%s\n",buffer );
-        
-        //clean buffer
-        bzero(buffer, sizeof(buffer));
-        //add exit condition
+    /*---------------------
+     Create/Join threads
+    ---------------------*/
+    int rc;
+    pthread_t threads[NUM_THREADS];
+    //printf("main(): creating input thread \n");
+    rc = pthread_create(&threads[0], NULL, &input_thread, NULL);
+    if(rc){
+        printf("Error: unable to create thread, %d \n", rc);
+        exit(-1);
     }
-	//send(sock , hello , strlen(hello) , 0 );
-	//printf("\nHello message sent\n");
-	//valread = read( sock , buffer, 1024);
-	//printf("%s\n",buffer );
-	return 0; 
+    //printf("main(): creating message thread \n");
+    rc = pthread_create(&threads[1], NULL, &message_thread, NULL);
+    if(rc){
+        printf("Error: unable to create thread, %d \n", rc);
+        exit(-1);
+    }
+    //join threads
+    for(int i = 0; i < NUM_THREADS; i++){
+        rc = pthread_join(threads[i], NULL);
+        if(rc){
+            printf("Joining Thread Error: %d \n", rc);
+        }
+    }
+    
+    
+    return 0;
 }
--- server.c
+++ server.c
@@ -1,55 +1,111 @@
-// Server side C/C++ program to demonstrate Socket programming 
+//===============================================================================================
+//HW 2:     Socket Programming
+//Author:   Florence Yao
+//Course:   CECS-327
+//Date:     2-25-19
+//Info:     Server side C/C++ program to demonstrate Socket programming. TCP is changed to non-
+//          blocking socket and runs a user input process in a separate thread. Sleep is used
+//          due to the loop running too fast to type inputs
+//===============================================================================================
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
 #include <stdlib.h> 
 #include <netinet/in.h> 
-#include <string.h> 
-#define PORT 8080 
-int main(int argc, char const *argv[]) 
-{ 
-	int server_fd, new_socket, valread;
-    //server_fd integer to hold socket descriptor it contains information about our socket
-    
-	struct sockaddr_in address; 
-	int opt = 1; 
-	int addrlen = sizeof(address); 
-	//char buffer[1024] = {0};
-	//char *hello = "Hello from server";
-	
-    //===================================================
-	// Creating socket file descriptor
-    //===================================================
+#include <string.h>
+#include <fcntl.h>
+#include <pthread.h>
+#define SENDER_NAME "server: "
+#define PORT 8080
+#define NUM_THREADS 2
+#define MSG_LEN 100
+#define BUF_SIZE 1024
+
+//GLOBAL
+int     new_socket;
+struct  sockaddr_in address;
+int     addrlen = sizeof(address);
+char    msg[MSG_LEN];
+char    buffer[BUF_SIZE] = {0};
+
+//==========================================================================
+// Concat two strings
+//==========================================================================
+void concat(char* p, char *q){
+   int c, d;
+   c = 0;
+   while (p[c] != '\0') {
+      c++;      
+   }
+   d = 0;
+   while (q[d] != '\0') {
+      p[c] = q[d];
+      d++;
+      c++;    
+   }
+   p[c] = '\0';
+}
+
+//==========================================================================
+// User Input Thread
+// Gets the message input from user.
+//==========================================================================
+void* input_thread(void* arg){
+    for(;;){
+        char label[] = SENDER_NAME;
+        fgets(msg, MSG_LEN, stdin);
+        concat(label, msg); //concat label to message to identify sender
+        strcpy(msg, label);
+    }                                      
+    pthread_exit(NULL);
+    return NULL;
+}
+
+//==========================================================================
+// Send/Receive Socket
+// Sends and receives messages to client. 
+// *note sleep() is used because without it, the loop appears to be too fast
+// and messages aren't being sent correctly
+//==========================================================================
+void* message_thread(void* arg){
+    for(;;){
+        int valread = recv( new_socket , buffer, BUF_SIZE, 0);
+        send(new_socket , msg , strlen(msg) , 0);
+        memset(msg, 0, MSG_LEN);
+        printf("%s", buffer);                         //display message
+        bzero(buffer, sizeof(buffer));                //flush buffer
+        sleep(1);                                     //introduce delay or else loops too fast (?)                                  
+    }
+    close(new_socket);
+    pthread_exit(NULL);
+    return NULL; //silence
+}
+
+//==========================================================================
+// Main connects sockets, creates and join threads
+// notes: AF_INET = domain of socket
+//        SOCK_STREAM = type of socket (TCP/UDP)
+//        0 = default protocol, TCP   */
+//==========================================================================
+int main(int argc, char const *argv[])
+{
+    int server_fd;
+    /*-------------------------------
+     Creating socket file descriptor
+    --------------------------------*/
 	if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0)
-    //AF_INET = domain of socket
-    //SOCK_STREAM = type of socket (TCP/UDP-datagram)
-    //0 = default protocol, TCP
 	{ 
 		perror("socket failed"); 
 		exit(EXIT_FAILURE); 
-	} 
-
-    //===================================================
-    // SetSockOpt
-    // Forcefully attaching socket to the port 8080
-    //===================================================
-//    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT,
-//                                                &opt, sizeof(opt)))
-//    {
-//        perror("setsockopt");
-//        exit(EXIT_FAILURE); 
-//    }
+    }
 	address.sin_family = AF_INET;
 	address.sin_addr.s_addr = INADDR_ANY; 
 	address.sin_port = htons( PORT ); 
 	
-	
-    //===================================================
-    // Bind
-    // Forcefully attaching socket to the port 8080
-    //===================================================
-	if (bind(server_fd, (struct sockaddr *)&address, 
-								sizeof(address))<0) 
+	/*-------------------------------------------------------------------
+      Bind - once you have a socket, bind() it to a port on your machine
+    -------------------------------------------------------------------*/
+	if (bind(server_fd, (struct sockaddr *)&address, sizeof(address))<0)
 	{ 
 		perror("bind failed"); 
 		exit(EXIT_FAILURE); 
@@ -57,54 +113,56 @@ int main(int argc, char const *argv[])
     else{
         printf("\nConnected!\n\n");
     }
-    //===================================================
-    // Listen
-    //===================================================
+
+    /*-------------------------------------------------------------------------
+      Listen - original socket server_fd listens for more incoming connections
+    -------------------------------------------------------------------------*/
 	if (listen(server_fd, 3) < 0) 
 	{ 
 		perror("listen"); 
 		exit(EXIT_FAILURE); 
 	}
-    
-    //===================================================
-    // Accept
-    //===================================================
+
+    /*-------------------------------------------------------------
+     Accept - someone is waiting on you to accept their connection
+     new_socket = the connection between you and the client
+    --------------------------------------------------------------*/
 	if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
 					(socklen_t*)&addrlen)) < 0)
 	{ 
 		perror("accept"); 
 		exit(EXIT_FAILURE); 
 	}
-    
-    //===================================================
-    // Send/Receive
-    //===================================================
-    char msg[50];
-    //char buffer[1024] = {0};
-    char buffer[1024] = {0};
-    
-    for(;;){
-        //wait for message in buffer (wait for message)
 
-        printf("client: ");
-        valread = read( new_socket , buffer, 1024);
-        
-        //print buffer
-        printf("%s\n",buffer);
-        
-        //put message in buffer (send message to client)
-        //printf(":");
-        //scanf ("%[^\n]%*c", msg);
-        //send(new_socket , msg , strlen(msg) , 0 );
-        
-        //clean buffer
-        bzero(buffer, sizeof(buffer));
-        //add exit condition
+    /*----------------------------------
+     Change the socket to non-blocking
+    -----------------------------------*/
+    fcntl(new_socket, F_SETFL, O_NONBLOCK);
+    fcntl(server_fd, F_SETFL, O_NONBLOCK);
+
+    /*--------------------
+     Create/Join threads
+    ---------------------*/
+    int rc;
+    pthread_t threads[NUM_THREADS];
+    //printf("main(): creating input thread \n");
+    rc = pthread_create(&threads[0], NULL, &input_thread, NULL);
+    if(rc){
+        printf("Error: unable to create thread, %d \n", rc);
+        exit(-1);
     }
-    
-    //valread = read( new_socket , buffer, 1024);
-    //printf("%s\n",buffer );
-    //send(new_socket , hello , strlen(hello) , 0 );
-    //printf("Hello message sent\n");
-	return 0; 
-} 
+    //printf("main(): creating message thread \n");
+    rc = pthread_create(&threads[1], NULL, &message_thread, NULL);
+    if(rc){
+        printf("Error: unable to create thread, %d \n", rc);
+        exit(-1);
+    }
+    for(int i = 0; i < NUM_THREADS; i++){
+        rc = pthread_join(threads[i], NULL);
+        if(rc){
+            printf("Joining Thread Error: %d \n", rc);
+        }
+    }
+
+    return 0;
+}
